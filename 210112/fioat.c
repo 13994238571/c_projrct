9f2ce04ff9e1067c835950226bd7a5f28ec88091
13994238571 <13994238571@163.com>
2021-02-12 12:13:33 +0800

    2101

---
+++ 210112/fioat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    float f1 = 123.456001;
+    float f2 = 123.456002;
+
+    if(f1 == f2) {
+    
+        printf("相等");
+    }else {
+        printf("不相等");
+    }
+    return 0;
+	// kk
+
+}
---
+++ 210123/unsigned.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    unsigned int a = 9;
+    int b = -4,c;
+    c = a/b;
+    printf("c = %d\n", c);
+
+
+}
