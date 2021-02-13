78c13c27dde277b35c23a091b78e0bfb68f99040
13994238571 <13994238571@163.com>
2021-02-13 15:17:41 +0800

    数组元素

---
+++ 210213/sum_arr1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define SIZE 10
+int main(int argc, char* argv[])
+{
+    int marbles[SIZE] = {20, 10, 5, 39, 4, 16, 19, 26, 31, 20};
+    long answer;
+    printf("%zd bytes\n", sizeof marbles);
+
+
+}
