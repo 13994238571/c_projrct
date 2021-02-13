c7b95fcbb24e17f233bce629e1b9ed77fb9dd232
13994238571 <13994238571@163.com>
2021-02-13 13:05:08 +0800

    指针地址

---
+++ 210213/pnt_add.c
@@ -0,0 +1,37 @@
+/*指针地址*/
+
+#include <stdio.h>
+#define SIZE 4
+int main(int argc, char* argv[])
+{
+    short dates[SIZE];
+    short* pti;
+    short index;
+    double bills[SIZE];
+    double* ptf;
+    pti = dates;
+    ptf = bills;
+    printf("%23s %15s\n","short", "double");
+    for(index = 0; index < SIZE; index++) {
+        printf("pointers + %d: %10p %10p\n", index, pti + index, ptf + index);
+
+
+
+    }
+    return 0;
+
+
+}
+
+/*
+                  short          double
+pointers + 0: 0x7ffd89c6a408 0x7ffd89c6a410
+pointers + 1: 0x7ffd89c6a40a 0x7ffd89c6a418
+pointers + 2: 0x7ffd89c6a40c 0x7ffd89c6a420
+pointers + 3: 0x7ffd89c6a40e 0x7ffd89c6a428
+指针+1指的是加一个存储单元。对数组而言，意味着加1后的地址是下一个元素的地址。
+int sum(int* ar, int n)
+int sum(int*, int)
+int sum(int ar[], int n)
+int sun(int [], int)
+*/
