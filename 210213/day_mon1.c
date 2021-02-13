16fe3beceb1e0452e1f1ec4aa5d213d3a78f601b
13994238571 <13994238571@163.com>
2021-02-13 08:21:20 +0800

    打印每个月天数

---
+++ 210213/day_mon1.c
@@ -0,0 +1,19 @@
+/*打印每个月的天数*/
+#include <stdio.h>
+#define MONTHS 12
+int main(int argc, char* argv[])
+{
+    int day[MONTHS] = {31,28,31,30,31,30,31,31,30,331,30,31,};
+    int index;
+    for(index = 0; index < MONTHS; index++) {
+        printf("month %2d has %2d day\n", index + 1, day[index]);
+        //return 0;
+
+    }
+
+
+
+
+
+
+}
